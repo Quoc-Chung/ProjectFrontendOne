3eb75f2b18ea2d9eacce384dc8c79fe6ae088dc7
QuocChung <tuquocchung@gmail.com>
2025-09-17 22:41:08 +0700

    project base

---
+++ src/redux/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { authReducer } from "./Client/Auth/AuthReducer";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+// Types
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Custom hooks
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
